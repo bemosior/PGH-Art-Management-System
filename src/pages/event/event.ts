0e29365009c5f9714f3e3ed80d8b654521a5905b
kurtnovack <kurt@walltowall.com>
2017-04-06 22:38:20 -0400

    better events and a checkin button

--- src/pages/event/event.ts
+++ src/pages/event/event.ts
@@ -1,13 +1,36 @@
 import { Component } from '@angular/core';
 
-import { NavController } from 'ionic-angular';
+import { Event } from '../../providers/event';
+import { EventService } from '../../providers/event.service';
+import { NavController, NavParams } from 'ionic-angular';
 
 @Component({
   selector: 'page-event',
   templateUrl: 'event.html'
 })
 export class EventPage {
-  constructor(private navCtrl: NavController) {
+  event: Event;
+  isGoing: boolean = false;
 
+  constructor(private navCtrl: NavController, private params: NavParams, private eventService: EventService) {}
+
+  getEvent(): void {
+    let id = this.params.get('id');
+    this.eventService.getEvent(id)
+      .subscribe(
+        event => this.event = event,
+        err => {
+          console.log(err);
+        },() => {
+          console.log(this.event);
+      });
+  }
+
+  checkIn(): void {
+    this.isGoing = true;
+  }
+
+  ngOnInit(): void {
+    this.getEvent();
   }
 }
