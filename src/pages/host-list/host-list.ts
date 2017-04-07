483f99458503185cb134c668466a18006cac1c53
kurtnovack <kurt@walltowall.com>
2017-04-06 21:48:34 -0400

    sets up hosts list

--- src/pages/host-list/host-list.ts
+++ src/pages/host-list/host-list.ts
@@ -1,13 +1,37 @@
 import { Component } from '@angular/core';
 
+import { Host } from '../../providers/host';
+import { HostService } from '../../providers/host.service';
 import { NavController } from 'ionic-angular';
+
+import { HostPage } from '../host/host'
 
 @Component({
   selector: 'page-host-list',
   templateUrl: 'host-list.html'
 })
 export class HostListPage {
-  constructor(private navCtrl: NavController) {
+  hosts: Host[];
 
+  constructor(private navCtrl: NavController, private hostService: HostService) {}
+
+  getHosts(): void {
+    this.hostService.getHosts()
+      .subscribe(
+        hosts => this.hosts = hosts,
+        err => {
+          console.log(err);
+        }
+      )
+  }
+
+  goToHost(hostId: number) {
+    this.navCtrl.push(HostPage, {
+      id: hostId
+    })
+  }
+
+  ngOnInit(): void {
+    this.getHosts();
   }
 }
